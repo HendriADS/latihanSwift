8f26c597faf35748d9ee379896bd12dc49defa4a
HendriADS <hendri@ayodyads.com>
2025-03-14 20:58:09 +0700

    add(reusable): reusable content (properties)

--- latihanSwift/YouTubeView.swift
+++ latihanSwift/YouTubeView.swift
@@ -50,97 +50,54 @@ struct YouTubeView: View {
 struct Content: View {
     var body: some View {
         List{
-            VStack(alignment: .leading) {
-                ZStack(alignment: .bottomTrailing){
-                    Image("content_1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                    Text("11:00:00")
-                        .font(.caption2)
-                        .padding(.all, 6)
-                        .foregroundStyle(.white)
-                        .background(Color.black.opacity(0.75))
-                        .clipShape(RoundedRectangle(cornerRadius: 6))
-                        .padding(.trailing, 5)
-                        .padding(.bottom, 5)
-                }
-                HStack(spacing: 20){
-                    Image("profile")
-                        .resizable()
-                        .clipShape(Circle())
-                        .frame(width: 35, height: 35)
-                    VStack(alignment: .leading){
-                        Text("Konten Video")
-                            .font(.system(size: 12, weight: .semibold))
-                        Text("SwiftChannel • 500rb x ditonton • 11 bulan yang lalu")
-                            .font(.system(size: 10, weight: .light))
-                    }
-                    Spacer()
-                    Image(systemName: "list.bullet")
-                        .foregroundStyle(.gray)
-                }
-            }
+            CellContent(imageContent: "content_1", duration: "11:00:00", imageProfile: "profile", titleVideo: "Konten Video", subTitleVideo: "SwiftChannel • 500rb x ditonton • 11 bulan yang lalu")
             
-            VStack(alignment: .leading) {
-                ZStack(alignment: .bottomTrailing){
-                    Image("content_2")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                    Text("11:00")
-                        .font(.caption2)
-                        .padding(.all, 6)
-                        .foregroundStyle(.white)
-                        .background(Color.black.opacity(0.75))
-                        .clipShape(RoundedRectangle(cornerRadius: 6))
-                        .padding(.trailing, 5)
-                        .padding(.bottom, 5)
-                }
-                HStack(spacing: 20){
-                    Image("profile")
-                        .resizable()
-                        .clipShape(Circle())
-                        .frame(width: 35, height: 35)
-                    VStack(alignment: .leading){
-                        Text("Konten Video")
-                            .font(.system(size: 12, weight: .semibold))
-                        Text("ChillsDev • 900rb x ditonton • 11 bulan yang lalu")
-                            .font(.system(size: 10, weight: .light))
-                    }
-                    Spacer()
-                    Image(systemName: "list.bullet")
-                        .foregroundStyle(.gray)
-                }
-            }
+            CellContent(imageContent: "content_2", duration: "20:00", imageProfile: "profile", titleVideo: "Chills Video", subTitleVideo: "chillOut • 1juta x ditonton • 11 bulan yang lalu")
             
-            VStack(alignment: .leading) {
-                ZStack(alignment: .bottomTrailing){
-                    Image("content_3")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                    Text("01:00:00")
-                        .font(.caption2)
-                        .padding(.all, 6)
-                        .foregroundStyle(.white)
-                        .background(Color.black.opacity(0.75))
-                        .clipShape(RoundedRectangle(cornerRadius: 6))
-                        .padding(.trailing, 5)
-                        .padding(.bottom, 5)
+            CellContent(imageContent: "content_3", duration: "50:00", imageProfile: "profile", titleVideo: "Swift UI Tutorial", subTitleVideo: "SwiftChannel • 20juta x ditonton • 2 tahun yang lalu")
+            
+        }
+    }
+}
+
+
+struct CellContent:View {
+    
+    var imageContent: String
+    var duration: String
+    var imageProfile: String
+    var titleVideo: String
+    var subTitleVideo: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            ZStack(alignment: .bottomTrailing){
+                Image(imageContent)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                Text(duration)
+                    .font(.caption2)
+                    .padding(.all, 6)
+                    .foregroundStyle(.white)
+                    .background(Color.black.opacity(0.75))
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+                    .padding(.trailing, 5)
+                    .padding(.bottom, 5)
+            }
+            HStack(spacing: 20){
+                Image(imageProfile)
+                    .resizable()
+                    .clipShape(Circle())
+                    .frame(width: 35, height: 35)
+                VStack(alignment: .leading){
+                    Text(titleVideo)
+                        .font(.system(size: 12, weight: .semibold))
+                    Text(subTitleVideo)
+                        .font(.system(size: 10, weight: .light))
                 }
-                HStack(spacing: 20){
-                    Image("profile")
-                        .resizable()
-                        .clipShape(Circle())
-                        .frame(width: 35, height: 35)
-                    VStack(alignment: .leading){
-                        Text("Konten Video")
-                            .font(.system(size: 12, weight: .semibold))
-                        Text("SwiftChannel • 500rb x ditonton • 11 bulan yang lalu")
-                            .font(.system(size: 10, weight: .light))
-                    }
-                    Spacer()
-                    Image(systemName: "list.bullet")
-                        .foregroundStyle(.gray)
-                }
+                Spacer()
+                Image(systemName: "list.bullet")
+                    .foregroundStyle(.gray)
             }
         }
     }
